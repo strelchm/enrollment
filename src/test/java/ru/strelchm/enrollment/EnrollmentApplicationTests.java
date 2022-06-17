7d00cf94022c89f35ea082916f9e23146fd5f816
strelchm <strelchm@yandex.ru>
2022-06-17 22:49:58 +0700

    Swagger schema generated

---
+++ src/test/java/ru/strelchm/enrollment/EnrollmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.strelchm.enrollment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrollmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
